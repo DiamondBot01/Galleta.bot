a3b153e40ca0a4add22fd9f8405abf31fbeacaff
Gαlletα.bot <143433583+DiamondBot01@users.noreply.github.com>
2023-08-31 02:38:55 -0300

    Update nat.js

--- nat.js
+++ nat.js
@@ -1,96 +1,128 @@
 const { 
-   WAConnection, 
-   MessageType, 
-   Presence,  
-   MessageOptions, 
-   Mimetype, 
-   WALocationMessage, 
-   WA_MESSAGE_STUB_TYPES, 
-   ReconnectMode, 
-   ProxyAgent, 
-   GroupSettingChange, 
-   ChatModification, 
-   waChatKey, 
-   WA_DEFAULT_EPHEMERAL, 
-   mentionedJid, 
-   prepareMessageFromContent,  
-   Browsers, 
-   processTime 
-} = require("@adiwajshing/baileys")
-const moment = require("moment-timezone");
-const os = require("os");
-const imageToBase64 = require('image-to-base64');
-const speed = require('performance-now');
-const chalk = require('chalk');
-const crypto = require("crypto-js");
-const CryptoJS = require("crypto-js");
+default: WAConnection,
+downloadContentFromMessage, 
+emitGroupParticipantsUpdate, 
+emitGroupUpdate,
+generateWAMessageContent, 
+generateWAMessage, 
+makeInMemoryStore, 
+prepareWAMessageMedia, 
+MediaType, 
+areJidsSameUser, 
+WAMessageStatus, 
+AuthenticationState, 
+GroupMetadata, 
+initInMemoryKeyStore,
+getContentType, 
+MiscMessageGenerationOptions, 
+useSingleFileAuthState, 
+BufferJSON, 
+WAMessageProto, 
+MessageOptions, 
+WAFlag, WANode,	
+WAMetric,	
+ChatModification, 
+MessageTypeProto, 
+WALocationMessage, 
+ReconnectMode, 
+WAContextInfo, 
+proto,	
+WAGroupMetadata, 
+ProxyAgent,	
+waChatKey, 
+MimetypeMap, 
+MediaPathMap, 
+WAContactMessage, 
+WAContactsArrayMessage, 
+WAGroupInviteMessage, 
+WATextMessage, 
+WAMessageContent, 
+WAMessage, 
+BaileysError, 
+WA_MESSAGE_STATUS_TYPE, 
+MediaConnInfo, 
+URL_REGEX, 
+WAUrlInfo, 
+WA_DEFAULT_EPHEMERAL, 
+WAMediaUpload, 
+mentionedJid, 
+processTime, 
+Browser, 
+MessageType, 
+Presence, 
+WA_MESSAGE_STUB_TYPES, 
+Mimetype, 
+relayWAMessage,	
+Browsers, 
+GroupSettingChange, 
+delay, 
+DisconnectReason, 
+WASocket, 
+getStream, 
+WAProto, 
+isBaileys, 
+AnyMessageContent, 
+fetchLatestBaileysVersion 
+} = require('@adiwajshing/baileys');
+
 const fs = require('fs');
-const { wait, h2k, generateMessageID, getGroupAdmins, banner, start, info, success, close } = require('./lib/functions')
-const { addBanned, unBanned, BannedExpired, cekBannedUser } = require('./lib/banned.js')
-const { getLevelingXp, getLevelingId, addLevelingXp, addLevelingLevel, addLevelingId, getLevelingLevel, getUserRank, addCooldown, leveltab } = require('./lib/leveling.js')
-const { removeBackgroundFromImageFile } = require('remove.bg');
-const { exec } = require('child_process');
-const ffmpeg = require('fluent-ffmpeg');
+const P = require('pino');
+const yts = require("yt-search");
+const cfonts = require("cfonts");
+const chalk = require('chalk')
 const axios = require('axios');
-const fetch = require('node-fetch');
-const request = require('request');
-const { validmove, setGame } = require("./lib/tictactoe");
-const simple = require('./lib/simple.js');
-const {y2mateA, y2mateV} = require('./lib/y2mate.js')
-const {sm330mfire} = require('./lib/mediafire.js')
-const { ssstik } = require("./lib/tiktok.js")
-const {fbDown} = require('./lib/fb.js') 
-const { isFiltered, addFilter } = require('./lib/antispam')
-const chatban = JSON.parse(fs.readFileSync('./src/ban.json'))
-const ban = JSON.parse(fs.readFileSync('./src/banned.json'))
-const crypt = fs.readFileSync('./crypt/imp.json')
-const {convertSticker} = require("./lib/swm.js")
-const antimedia = JSON.parse(fs.readFileSync('./src/antimedia.json'))
-const antifake = JSON.parse(fs.readFileSync('./src/antifake.json'))
-const antibot = JSON.parse(fs.readFileSync('./src/antibot.json'))
-const bad = JSON.parse(fs.readFileSync('./src/bad.json'))
-const badword = JSON.parse(fs.readFileSync('./src/badword.json'))
-const autostick = JSON.parse(fs.readFileSync('./src/autostick.json'))
-const antilink = JSON.parse(fs.readFileSync('./src/antilink.json'))
-const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
-const config = JSON.parse(fs.readFileSync("./config.json"))
-const owner = config.owner 
-const mods = config.mods
-var public = config.public
-conn.connect()
-let _level = JSON.parse(fs.readFileSync('./src/level.json'))
-const _registered = JSON.parse(fs.readFileSync('./src/registered.json'))
-const daily = JSON.parse(fs.readFileSync('./src/diario.json'));
-const dailiy = JSON.parse(fs.readFileSync('./src/limitem.json'));
-const X = "❌️"
-const O = "⭕️"
+const speed = require("performance-now");
+const { getBuffer, getRandom, getExtension } = require('./archivos/lib/functions.js');
+const { fetchJson } = require("./archivos/lib/fetcher")
+// CONSTANTES SETTING //
 
-//========= Funcion de Registro =========\\
-const getRegisteredRandomId = () => {
-           return _registered[Math.floor(Math.random() * _registered.length)].id
+var NombreBot = 'Galleta-Bot'  
+var Creador = "Nataly" 
+
+// BANNER //
+
+const color = (text, color) => {
+return !color ? chalk.white(text) : chalk.keyword(color)(text)
 }
-const addRegisteredUser = (userid, sender, age, time, serials) => {
-const obj = { id: userid, name: sender, age: age, time: time, serial: serials }
-        _registered.push(obj)
-        fs.writeFileSync('./src/registered.json', JSON.stringify(_registered))
+const banner = cfonts.render(('Galleta|Bot'), {
+font : "block",
+align: "center",
+colors: ["blue","white"]
+})
+
+// CONEXION DEL BOT
+
+const store = makeInMemoryStore({ logger: P().child({ level: 'debug', stream: 'linhaDoTempo'}) })
+const { state, saveState } = useSingleFileAuthState('./cache/anita.json')
+async function startnat() {
+const { version, isLatest } = await fetchLatestBaileysVersion()
+console.log(`ðŸ’¤..Conectando..!!`)
+console.log(banner.string)
+console.log("..Galleta Bot..\nConectado Exitosamente..")
+const Galleta = WAConnection({
+logger: P({ level: "silent" }),
+printQRInTerminal: true,
+. browser: ['Galleta.bot, 'Nat', '1.0.0'],
+auth: state
+})
+anita.ev.on ("creds.update", saveState)
+store.bind(galleta.ev)
+anita.ev.on("chats.set", () => {
+console.log("Tem conversas", store.chats.all())
+})
+anita.ev.on("contacts.set", () => {
+console.log("Tem contatos", Object.values(store.contacts))
+})
+anita.ev.on("connection.update", (update) => {
+const { connection, lastDisconnect } = update
+if(connection === "close") {
+const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
+console.log("ðŸ˜ª..ConecciÃ³n inestable..", lastDisconnect.error, "Intentando conectar...", shouldReconnect);
+if(shouldReconnect) {
+startnat()
 }
-
-        const createSerial = (size) => {
-                   return crypto.randomBytes(size).toString('hex').slice(0, size)
-   }
-
-        const checkRegisteredUser = (sender) => {
-                   let status = false
-                   Object.keys(_registered).forEach((i) => {
-                              if (_registered[i].id === sender) {
-                                         status = true
-                                                    }
-                              })
-                       return status
-           
-           
-   
-
-
-
-   
+} else if(connection === "open") {
+console.log("Galleta-Bot funciona correctamente!!!")
+}
+})
+//*******************************************//
